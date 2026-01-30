53b85c5d946710302f39a8ab1013e48fe6bec836
Omar Maldonado <144171824+Omar-B-Maldonado@users.noreply.github.com>
2026-01-30 16:35:20 -0500

    Created SpringBoot + JPA + Lombok skeleton

---
+++ src/main/java/io/github/omar_b_maldonado/ipod_shuffler/IpodShufflerApplication.java
@@ -0,0 +1,13 @@
+package io.github.omar_b_maldonado.ipod_shuffler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpodShufflerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpodShufflerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/omar_b_maldonado/ipod_shuffler/IpodShufflerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.omar_b_maldonado.ipod_shuffler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpodShufflerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
